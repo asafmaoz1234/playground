e60e525332c2fbe988f88649144bbeff960fd918
asaf_m <asafmaoz27@gmail.com>
2018-05-01 11:25:34 +0300

    init project, setup classes for equalAndHash

---
+++ src/main/java/com/asaf/maoz/playground/equalandhash/Employee.java
@@ -0,0 +1,10 @@
+package com.asaf.maoz.playground.equalandhash;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private int id;
+    private String name;
+
+}
---
+++ src/main/java/com/asaf/maoz/playground/equalandhash/Manager.java
@@ -0,0 +1,4 @@
+package com.asaf.maoz.playground.equalandhash;
+
+public class Manager {
+}
